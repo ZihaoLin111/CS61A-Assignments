9b4c87f313e4dc9c87dc1eef55ceea57ccbe2f3f
Zihao Lin <216423917+ZihaoLin111@users.noreply.github.com>
2025-07-31 19:32:40 +0800

    lab03 finished

--- Lab/lab03/lab03.py
+++ Lab/lab03/lab03.py
@@ -15,6 +15,9 @@ def print_if(s, f):
     """
     for x in s:
         "*** YOUR CODE HERE ***"
+        if f(x) == True:
+            print(x)
+    pass
 
 
 def close(s, k):
@@ -33,6 +36,8 @@ def close(s, k):
     count = 0
     for i in range(len(s)):  # Use a range to loop over indices
         "*** YOUR CODE HERE ***"
+        if i <= s[i] + k and i >= s[i] - k:
+            count += 1
     return count
 
 
@@ -47,7 +52,7 @@ def close_list(s, k):
     >>> close_list(t, 2)  # 2, 3, 4, and 5 are all within 2 of their index
     [2, 4, 3, 5]
     """
-    return [___ for i in range(len(s)) if ___]
+    return [s[i] for i in range(len(s)) if i <= s[i] + k and i >= s[i] - k]
 
 
 from math import sqrt
@@ -63,7 +68,7 @@ def squares(s):
     >>> squares(seq)
     []
     """
-    return [___ for n in s if ___]
+    return [int(sqrt(n)) for n in s if sqrt(n) % 1 == 0]
 
 
 def double_eights(n):
@@ -88,6 +93,17 @@ def double_eights(n):
     True
     """
     "*** YOUR CODE HERE ***"
+    def is_eight(n):
+        if n % 10 == 8:
+            return True
+        else:
+            return double_eights(n // 10)
+    if n < 10:
+        return False    
+    elif n % 10 == 8:
+        return is_eight(n // 10)
+    else:
+        return double_eights(n // 10)
 
 
 def make_onion(f, g):
@@ -116,10 +132,10 @@ def make_onion(f, g):
     """
     def can_reach(x, y, limit):
         if limit < 0:
-            return ____
+            return False
         elif x == y:
-            return ____
+            return True
         else:
-            return can_reach(____, ____, limit - 1) or can_reach(____, ____, limit - 1)
+            return can_reach(f(x), y, limit - 1) or can_reach(g(x), y, limit - 1)
     return can_reach
 
