cc81cc7811b1ee5ff5061d895b3a2defff6605df
Zihao Lin <216423917+ZihaoLin111@users.noreply.github.com>
2025-09-13 01:32:24 +0800

    lab 09 finished

--- Lab/lab09/lab09.scm
+++ Lab/lab09/lab09.scm
@@ -1,10 +1,18 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) (cond
+                                      ((< num1 num2) -1)
+                                      ((= num1 num2) 0)
+                                      ((> num1 num2) 1)
+))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) (lambda (inc) (+ inc num)))
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) (lambda (x) (f (g x))))
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n) (define (repeated f n x)
+  (if (> n 1)
+    (repeated f (- n 1) (f x))
+    (f x)))
+    (lambda (x) (repeated f n x)))
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +24,7 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b) 
+  (if (zero? (min a b))
+      (max a b)
+      (gcd (min a b) (modulo (max a b) (min a b)))))
